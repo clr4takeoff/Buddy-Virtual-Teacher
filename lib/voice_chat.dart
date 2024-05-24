3cf981061269681b9f4fcc0491a5351f684f35a7
clr4takeoff <jetblue22@o.cnu.ac.kr>
2024-05-24 11:27:19 +0900

    feat: Change mic button color when pressed

--- lib/voice_chat.dart
+++ lib/voice_chat.dart
@@ -1,6 +1,25 @@
 import 'package:flutter/material.dart';
 
-class VoiceChat extends StatelessWidget {
+class VoiceChat extends StatefulWidget {
+  @override
+  _VoiceChatState createState() => _VoiceChatState();
+}
+
+class _VoiceChatState extends State<VoiceChat> {
+  bool _isPressed = false;
+
+  void _onMicButtonPress() {
+    setState(() {
+      _isPressed = true;
+    });
+  }
+
+  void _onMicButtonRelease() {
+    setState(() {
+      _isPressed = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,43 +35,52 @@ class VoiceChat extends StatelessWidget {
                   IconButton(
                     icon: Icon(Icons.close, color: Colors.black54),
                     iconSize: 30.0,
-                    onPressed: () {
-                    },
+                    onPressed: () {},
                   ),
                   SizedBox(width: 24),
                 ],
               ),
             ),
-            Spacer(),
-            Text(
-              '이야기를 시작해볼까?',
-              style: TextStyle(
-                color: Color(0xFF525252),
-                fontSize: 25,
-                fontFamily: 'Inter',
-                fontWeight: FontWeight.w700,
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 30.0),
+              child: Text(
+                '이야기를 시작해볼까?',
+                style: TextStyle(
+                  color: Color(0xFF525252),
+                  fontSize: 25,
+                  fontFamily: 'Inter',
+                  fontWeight: FontWeight.w700,
+                ),
               ),
             ),
-            SizedBox(height: 20),
-            Image.asset(
-              'assets/character1.png', // Ensure you have the image in the correct path
-              height: 300,
-            ),
-            SizedBox(height: 20),
-            Text(
-              '루피',
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-                color: Color(0xFFFD6967),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10.0),
+              child: Image.asset(
+                'assets/character1.png',
+                height: 300,
               ),
             ),
-            Text(
-              '뽀롱뽀롱뽀로로',
-              style: TextStyle(
-                fontSize: 25,
-                fontWeight: FontWeight.normal,
-                color: Colors.grey,
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10.0),
+              child: Column(
+                children: [
+                  Text(
+                    '루피',
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFFFD6967),
+                    ),
+                  ),
+                  Text(
+                    '뽀롱뽀롱뽀로로',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.normal,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
               ),
             ),
             Spacer(),
@@ -74,7 +102,7 @@ class VoiceChat extends StatelessWidget {
         ),
         child: BottomAppBar(
           child: Container(
-            height: 80.0,
+            height: 60.0,
             padding: EdgeInsets.symmetric(horizontal: 30.0),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -99,17 +127,25 @@ class VoiceChat extends StatelessWidget {
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: RawMaterialButton(
-        onPressed: () {},
-        elevation: 2.0,
-        fillColor: Color(0xFFFED43E),
-        child: Icon(
-          Icons.mic,
-          size: 70.0,
-          color: Colors.white,
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(bottom: 10),
+        child: GestureDetector(
+          onTapDown: (_) => _onMicButtonPress(),
+          onTapUp: (_) => _onMicButtonRelease(),
+          onTapCancel: _onMicButtonRelease,
+          child: RawMaterialButton(
+            onPressed: () {},
+            elevation: 2.0,
+            fillColor: _isPressed ? Color(0xFFFF9900) : Color(0xFFFED43E),
+            child: Icon(
+              Icons.mic,
+              size: 70.0,
+              color: Colors.white,
+            ),
+            padding: EdgeInsets.all(22.0),
+            shape: CircleBorder(),
+          ),
         ),
-        padding: EdgeInsets.all(22.0),
-        shape: CircleBorder(),
       ),
     );
   }
